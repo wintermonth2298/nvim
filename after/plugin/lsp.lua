53d26902866f56b4703a9587f6201379a2a5f669
wintermonth2298 <wintermonth@mail.ru>
2023-03-27 14:01:40 +0300

    fix diagnostics binding

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,8 +4,8 @@ local lspconfig = require('lspconfig')
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '<leader>n', vim.diagnostic.goto_prev)
-vim.keymap.set('n', '<leader>p', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>p', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>n', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
 local on_attach = function(ev)
