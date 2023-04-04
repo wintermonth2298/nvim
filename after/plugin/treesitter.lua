03d9c00d83f04e31f1869f9ae5379689d9e93439
mallakhverdiev <mallakhverdiev@ozon.ru>
2023-04-04 17:08:26 +0300

    add textobkects bindings for block

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -8,6 +8,16 @@ require 'nvim-treesitter.configs'.setup {
     -- List of parsers to ignore installing (for "all")
     ignore_install = { "javascript" },
 
+    textobjects = {
+        select = {
+            keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ib"] = "@block.inner",
+                ["ab"] = "@block.outer",
+            },
+        },
+    },
     highlight = {
         -- `false` will disable the whole extension
         enable = true,
