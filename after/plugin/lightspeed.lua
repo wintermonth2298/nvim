dec83604649cccdcfc5af24d5ba0ff04a1f5dcf8
wintermonth2298 <wintermonth1@gmail.com>
2023-04-10 23:07:17 +0300

    fix lightspeed and make lsp updat on insert

---
+++ after/plugin/lightspeed.lua
@@ -0,0 +1,4 @@
+vim.cmd [[unmap f]]
+vim.cmd [[unmap F]]
+vim.cmd [[unmap t]]
+vim.cmd [[unmap T]]
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -89,7 +89,7 @@ end
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
         underline = true,
-        update_in_insert = false,
+        update_in_insert = true,
         virtual_text = { spacing = 4, prefix = "●" },
         severity_sort = true,
     }
