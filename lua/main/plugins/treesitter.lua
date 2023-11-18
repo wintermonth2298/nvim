661b4dc4bc63cc2e26de0f05ccc4e1e1a66c64ca
wintermonth2298 <wintermonth1@gmail.com>
2023-11-18 22:19:28 +0300

    Add treesitter-context

--- lua/main/plugins/treesitter.lua
+++ lua/main/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require 'nvim-treesitter.configs'.setup {
-            ensure_installed = { "go", "python", "lua", "markdown", "markdown_inline", "html", "json" },
+            ensure_installed = { "go", "python", "lua", "markdown", "markdown_inline", "html", "json", "yaml" },
             auto_install = true,
             modules = {},
             sync_install = false,
