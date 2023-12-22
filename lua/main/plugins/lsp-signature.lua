1bb0792da65fc69dfaa6339307a6a4ff4a4bd4eb
wintermonth2298 <wintermonth1@gmail.com>
2023-12-23 02:01:12 +0300

    Remove doclines settings, not working

--- lua/main/plugins/lsp-signature.lua
+++ lua/main/plugins/lsp-signature.lua
@@ -4,7 +4,6 @@ return {
     opts = {
         handler_opts = { border = "rounded" },
         hint_prefix = "",
-        doc_lines = 0,
         hint_enable = false,
     },
     config = function(_, opts) require 'lsp_signature'.setup(opts) end
