0d74177ed43035036a6e8927e87eee3645ce57b6
wintermonth2298 <wintermonth1@gmail.com>
2024-02-19 12:28:44 +0300

    Make nvim-worktree more flexible

--- lua/main/plugins/git-worktree.lua
+++ lua/main/plugins/git-worktree.lua
@@ -1,5 +1,9 @@
 return {
     "ThePrimeagen/git-worktree.nvim",
+    opts = {
+        change_directory_command = "lcd",
+        update_on_change = false,
+    },
     keys = function()
         return {
             { "<leader>tw", require('telescope').extensions.git_worktree.git_worktrees,       silent = true, desc = "Worktrees show" },
