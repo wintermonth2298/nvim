3bcbbaa16479fa31dfea145abf32dafd4d90af24
mallakhverdiev <mallakhverdiev@ozon.ru>
2024-03-16 00:26:20 +0300

    Fix fugitive toggle

--- lua/main/plugins/fugitive.lua
+++ lua/main/plugins/fugitive.lua
@@ -1,22 +1,5 @@
-local function find_buffer_with_regex(regex)
-    local buffers = vim.api.nvim_list_bufs()
-    for _, bufnr in ipairs(buffers) do
-        local name = vim.api.nvim_buf_get_name(bufnr)
-        if name:match(regex) then
-            return bufnr
-        end
-    end
-    return nil
-end
-
-
 local function toggle()
-    local fugitive_buf_no = find_buffer_with_regex('^fugitive:')
-    if fugitive_buf_no == nil then
-        vim.cmd(":G")
-        return
-    end
-
+    local fugitive_buf_no = vim.fn.bufnr('^fugitive:')
     local buf_win_id = vim.fn.bufwinid(fugitive_buf_no)
     if fugitive_buf_no >= 0 and buf_win_id >= 0 then
         vim.api.nvim_win_close(buf_win_id, false)
