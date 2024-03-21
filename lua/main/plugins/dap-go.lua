1c7328c8d3b77d1fdc0bd18155524216a9a45f8e
wintermonth2298 <wintermonth1@gmail.com>
2024-03-21 23:36:22 +0300

    Dapui add dependency

--- lua/main/plugins/dap-go.lua
+++ lua/main/plugins/dap-go.lua
@@ -23,6 +23,7 @@ return {
         'mfussenegger/nvim-dap',
         'rcarriga/nvim-dap-ui',
         'theHamsta/nvim-dap-virtual-text',
+        "nvim-neotest/nvim-nio",
     },
     config = function()
         require("dapui").setup()
